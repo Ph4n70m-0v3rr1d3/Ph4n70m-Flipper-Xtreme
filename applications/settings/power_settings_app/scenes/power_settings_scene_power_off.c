788285ea10c10c06ebe7907f68166a61c267c02e
Ph4n70m-0v3rr1d3 <ph4n70m_0v3rr1d3@proton.me>
2023-05-07 15:20:38 -0500

    changed power off message, ya skid!

--- applications/settings/power_settings_app/scenes/power_settings_scene_power_off.c
+++ applications/settings/power_settings_app/scenes/power_settings_scene_power_off.c
@@ -17,7 +17,7 @@ void power_settings_scene_power_off_on_enter(void* context) {
             dialog, "   I will be\nwaiting for\n you master", 78, 16, AlignLeft, AlignTop);
     } else {
         dialog_ex_set_text(
-            dialog, "   I will be\nwaiting for\n you here", 78, 16, AlignLeft, AlignTop);
+            dialog, "   GTFO here\n   You Skid", 78, 16, AlignLeft, AlignTop);
     }
     dialog_ex_set_icon(dialog, 21, 13, XTREME_ASSETS()->I_Cry_dolph_55x52);
     dialog_ex_set_left_button_text(dialog, "Back");
